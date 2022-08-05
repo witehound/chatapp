c773014c98c47d350ae43cd323feb59e01650bbd
InventedMost <netvid00@gmail.com>
2022-08-05 08:57:54 +0100

    debbuging chat update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,189 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import Picker from "emoji-picker-react";
+const socket = io("http://localhost:5000");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socketId, setSocketId] = useState("");
+  const [message, setMessage] = useState("");
+  const [users, setUsers] = useState([]);
+  const [rooms, setRooms] = useState<any>([]);
+  const [joinedRoom, setJoinedRoom] = useState(false);
+  const [room, setRoom] = useState<String>("");
+  const [chat, setChat] = useState([]);
+  const [showEmoji, setShowEmoji] = useState(false);
+  //Emoji
+
+  const onEmojiClick = (event: any, emojiObject: any) => {
+    setMessage(message + emojiObject.emoji);
+  };
+  // scroll
+  const chatContainer = useRef<any>(null);
+
+  useEffect(() => {
+    socket.on("me", (id) => {
+      setSocketId(id);
+    });
+
+    socket.on("disconnect", () => {
+      socket.disconnect();
+    });
+
+    socket.on("getAllUsers", (users) => {
+      setUsers(users);
+    });
+    // Real time
+    socket.on("updateUsers", (users) => {
+      setUsers(users);
+    });
+
+    socket.on("getAllRooms", (rooms) => {
+      setRooms(rooms);
+    });
+    // Real time
+    socket.on("updateRooms", (rooms) => {
+      setRooms(rooms);
+    });
+
+    // Rooms
+
+    if (joinedRoom === true) {
+      chatContainer.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [chat, rooms, message]);
+
+  const sendMessage = async () => {
+    const payload = { message: message, room: room, socketId: socketId };
+    socket.emit("message", payload);
+    setMessage("")
+    chatContainer.current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+    socket.on("getAllRooms", (rooms) => {
+      setRooms(rooms);
+    });
+    // Real time
+    socket.on("updateRooms", (rooms) => {
+      setRooms(rooms);
+    });
+    console.log(rooms)
+  };
+
+  const createRoom = () => {
+    socket.emit("createRoom");
+    socket.on("getRoom", (room) => {
+      setRooms([...rooms, room]);
+    });
+  };
+
+  const joinRoom = (room: any) => {
+    socket.emit("joinRoom", room);
+    setRoom(room.id);
+    setJoinedRoom(true);
+    setChat(room.chat);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <h1 className="main_heading">Chat App</h1>
+      <h1 className="my_socket">Me: {socketId}</h1>
+      <h3 className="roomjoined">
+        {joinedRoom === true
+          ? `Room: ${room}`
+          : "You are not joined in any room"}
+      </h3>
+
+      {!joinedRoom && (
+        <div className="container">
+          <div className="users-container">
+            <h2 className="users_heading">Online Users:</h2>
+            <ul className="users">
+              {users.map((user) => {
+                return (
+                  <li className="user" key={user}>
+                    {user && user === socketId ? `*ME*` : user}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="rooms-container">
+            <h2 className="rooms_heading">Available Rooms:</h2>
+
+            {rooms.length === 0 ? (
+              <h3 className="no_rooms">No Rooms! Create a room !</h3>
+            ) : (
+              <ul className="rooms">
+                {rooms.map((room: any) => {
+                  return (
+                    <li key={room.id} onClick={() => joinRoom(room)}>
+                      {room.id}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            <div className="btn-container">
+              <button className="btn" onClick={() => createRoom()}>
+                Create Room
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {joinedRoom && (
+        <>
+          <div className="chat-container">
+            <ul className="chat-list" id="chat-list" ref={chatContainer}>
+              {chat.map((chat: any, idx) => (
+                <li
+                  key={idx}
+                  className={chat.writer === socketId ? "chat-me" : "chat-user"}
+                >
+                  {chat.writer === socketId
+                    ? `${chat.message}: ME*`
+                    : `User (${chat.writer.slice(0, 5)}): ${chat.message}`}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <form className="chat-form" onSubmit={(e) => e.preventDefault()}>
+            <input
+              type="text"
+              placeholder="Your message ..."
+              autoFocus
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              value={message}
+            />
+
+            <button
+              className="emoji_btn"
+              type="button"
+              onClick={() => setShowEmoji(!showEmoji)}
+            >
+              Emoji
+            </button>
+            <button
+              className="send_btn"
+              type="submit"
+              onClick={() => sendMessage()}
+            >
+              Send
+            </button>
+          </form>
+        </>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
